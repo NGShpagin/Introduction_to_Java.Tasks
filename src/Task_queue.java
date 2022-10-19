e507a9afdaddd4cdf8c63dc47862196278e762b4
NGShpagin <shpagin9393@mail.ru>
2022-10-19 16:07:28 +0300

    add method DeriveAllEl

--- src/Task_queue.java
+++ src/Task_queue.java
@@ -2,16 +2,18 @@ import java.util.Arrays;
 
 public class Task_queue {
     public static void main(String[] args) {
-        int[] array = new int[]{1,2,3,4};
+        int[] array = new int[]{1, 2, 3, 4, 3};
         System.out.println(Arrays.toString(array));
         array = addFirst(array, 1);
         array = addLast(array, 9);
+        System.out.println(Arrays.toString(array));
+        array = deriveAllEl(array, 3);
         System.out.println(Arrays.toString(array));
     }
 
     public static int[] addFirst(int[] array, int element) {
         int[] newArr = new int[]{};
-        newArr = Arrays.copyOf(array, array.length+1);
+        newArr = Arrays.copyOf(array, array.length + 1);
         System.out.println(Arrays.toString(newArr));
         System.arraycopy(array, 0, newArr, 1, array.length);
         newArr[0] = element;
@@ -20,10 +22,27 @@ public class Task_queue {
 
     public static int[] addLast(int[] array, int element) {
         int[] newArr = new int[]{};
-        newArr = Arrays.copyOf(array, array.length+1);
+        newArr = Arrays.copyOf(array, array.length + 1);
         System.out.println(Arrays.toString(newArr));
         System.arraycopy(array, 0, newArr, 0, array.length);
-        newArr[newArr.length-1] = element;
+        newArr[newArr.length - 1] = element;
         return newArr;
+    }
+
+    // Извлекает из массива все вхождения элемента
+    public static int[] deriveAllEl(int[] array, int element) {
+        int[] newArr = new int[array.length];
+        int index = 0;
+        int count = 0; // кол-во вхождений искомого элемента в массив
+        for (int j : array) {
+            if (j == element) count++;
+            else {
+                newArr[index] = j;
+                index++;
+            }
+        }
+        int[] newArr1 = new int[newArr.length-count];
+        System.arraycopy(newArr, 0, newArr1, 0, newArr1.length);
+        return newArr1;
     }
 }
